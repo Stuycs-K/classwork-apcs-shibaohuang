98070e8db7e9bcfba9854409701b517992b9c769
Skylar Huang <shibaoh@nycstudents.net>
2024-09-27 11:37:45 -0400

    Added the arrayToString method

---
+++ 2024-09-27/MyArrays.java
@@ -0,0 +1,16 @@
+public class MyArrays{
+  public static String arrayToString(int[] nums){
+    String ans = "[";
+    for (int i = 0; i < nums.length; i++){
+      ans = ans + nums[i];
+      if (i < nums.length - 1){
+        ans = ans + ", ";
+      }
+    }
+    return ans + "]";
+  }
+
+  public static void main(String[args]){
+    
+  }
+}
