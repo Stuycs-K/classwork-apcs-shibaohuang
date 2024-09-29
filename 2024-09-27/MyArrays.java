d9e6ba785509d672a45f2de8dfb50dcb838450c8
Skylar Huang <shibaoh@nycstudents.net>
2024-09-29 11:35:25 -0400

    Added concatArray method and its test cases

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -18,6 +18,17 @@ public class MyArrays{
     return copy;
   }
 
+  public static int[] concatArray(int[] ary1, int[] ary2){
+    int[] concatenated = new int[ary1.length + ary2.length];
+    for (int i = 0; i < ary1.length; i++){
+      concatenated[i] = ary1[i];
+    }
+    for (int i = 0; i < ary2.length; i++){
+      concatenated[ary1.length + i] = ary2[i];
+    }
+    return concatenated;
+  }
+
   public static void main(String[] args){
     int[] array = {1, 2, 3, 4};
     int[] copy = returnCopy(array);
@@ -30,5 +41,20 @@ public class MyArrays{
     array = new int[] {};
     copy = returnCopy(array);
     System.out.println("Arrays match and are different arrays: " + (arrayToString(array).equals(arrayToString(copy)) && (array != copy)));
+  
+    int[] array1 = {1, 2, 3};
+    int[] array2 = {4, 5, 6};
+    int[] concatenated = concatArray(array1, array2);
+    System.out.println("Concatenated array matches expected result: " + arrayToString(concatenated).equals("[1, 2, 3, 4, 5, 6]"));
+
+    array1 = new int[] {4, 5, 6};
+    array2 = new int[] {4, 5, 6};
+    concatenated = concatArray(array1, array2);
+    System.out.println("Concatenated array matches expected result: " + arrayToString(concatenated).equals("[4, 5, 6, 4, 5, 6]"));
+
+    array1 = new int[] {};
+    array2 = new int[] {};
+    concatenated = concatArray(array1, array2);
+    System.out.println("Concatenated array matches expected result: " + arrayToString(concatenated).equals("[]"));
   }
 }
