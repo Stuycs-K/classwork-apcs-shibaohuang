3a3be04e9253f0ed6ccc4ca618e3c9d4f3a66051
Skylar Huang <shibaoh@nycstudents.net>
2024-10-01 22:17:40 -0400

    Copied replaceNegative method and test cases from school

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -56,6 +56,19 @@ public class ArrayMethods {
     return swapped;
   }
 
+    public static void replaceNegative(int[][] vals) {
+    for (int i = 0; i < vals.length; i++) {
+      for (int j = 0; j < vals[i].length; j++) {
+        if (vals[i][j] < 0) {
+          if (i == j) {
+            vals[i][j] = 1;
+          } else {
+            vals[i][j] = 0;
+          }
+        }
+      }
+    }
+  }
 
   public static void main (String[] args) {
     // test arrayToString int[]
@@ -128,5 +141,25 @@ public class ArrayMethods {
       {3, 4, 7}
     })));
 
+    // test replaceNegative int[][]
+    int[][] testArray2D = new int[][] {
+      {2, 3, -4},
+      {-5, -9, -7},
+      {2, 4, 0}
+    };
+
+    replaceNegative(testArray2D);
+
+    System.out.println(arrToString(testArray2D));
+
+    testArray2D = new int[][] {
+      {0, 3, -4},
+      {8, -9, -9, -7},
+      {}
+    };
+
+    replaceNegative(testArray2D);
+
+    System.out.println(arrToString(testArray2D));
   }
 }
