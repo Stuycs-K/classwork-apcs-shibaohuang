52bc9d13c14887eb5ebacc97a91aa2cff40f1ec6
Skylar Huang <shibaoh@nycstudents.net>
2024-10-01 22:44:36 -0400

    Added copy method and test cases for it

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -41,7 +41,7 @@ public class ArrayMethods {
     return out;
   }
 
-  public static int[][] swapRC(int[][]nums){
+  public static int[][] swapRC(int[][]nums) {
     if (nums.length == 0) {
         return new int[0][0];
     }
@@ -69,6 +69,22 @@ public class ArrayMethods {
       }
     }
   }
+
+  public static int[][] copy(int[][] nums) {
+    int[][] copyArray = new int[nums.length][];
+    for (int i = 0; i < nums.length; i++) {
+        copyArray[i] = copy1D(nums[i]);
+    }
+    return copyArray;
+  }
+
+  public static int[] copy1D(int[] arr) {
+    int[] copy = new int[arr.length];
+    for (int i = 0; i < arr.length; i++) {
+        copy[i] = arr[i];
+    }
+    return copy;
+}
 
   public static void main (String[] args) {
     // test arrayToString int[]
@@ -161,5 +177,39 @@ public class ArrayMethods {
     replaceNegative(testArray2D);
 
     System.out.println(arrToString(testArray2D));
+
+    // test copy int[][]
+    int[][] originalArray = new int[][] {
+      {2, 3, 4},
+      {5, 6, 7},
+      {2, 4, 0}
+    };
+
+    int[][] copiedArray = copy(originalArray);
+
+    System.out.println("Original array: " + arrToString(originalArray));
+    System.out.println("Copied array: " + arrToString(copiedArray));
+
+    originalArray[0][0] = 99;
+    System.out.println("Original array: " + arrToString(originalArray));
+    System.out.println("Copied array: " + arrToString(copiedArray));
+
+    int[][] emptyArray = new int[][] {};
+    int[][] copiedEmptyArray = copy(emptyArray);
+    System.out.println("Original array: " + arrToString(emptyArray));
+    System.out.println("Copied and modified array: " + arrToString(copiedEmptyArray));
+
+    int[][] mixedArray = new int[][] {
+      {0, -3, 5},
+      {},
+      {7, 8}
+    };
+    int[][] copiedMixedArray = copy(mixedArray);
+    System.out.println("Original array: " + arrToString(mixedArray));
+    System.out.println("Copied array: " + arrToString(copiedMixedArray));
+
+    mixedArray[1] = new int[] {42};
+    System.out.println("Original array: " + arrToString(mixedArray));
+    System.out.println("Copied and modified array: " + arrToString(copiedMixedArray));
   }
 }
