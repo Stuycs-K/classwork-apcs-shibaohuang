36c8c1e6b1182e52c96479d973b5a1a3152be56d
Skylar Huang <shibaoh@nycstudents.net>
2024-10-07 11:46:22 -0400

    added new methods

--- 2024-10-07-2DArrayMethods/ArrayDemo.java
+++ 2024-10-07-2DArrayMethods/ArrayDemo.java
@@ -2,10 +2,12 @@ import java.util.Arrays;
 public class ArrayDemo {
 
   public static void main(String[]args){
-
-
+    //write your tests here!
+    //You can now use Arrays.toString(yourArray) instead of writing arrayToString again.
+    //Compare Arrays.toString(yourArray) to YOUR arrayToString() method to make sure yours is correct
+    //do not use any other Arrays.method()
   }
-  
+
   public static String arrToString(int[] ary) {
     String s = "[";
 
@@ -24,10 +26,56 @@ public class ArrayDemo {
     return s;
   }
 
+  public static String arrToString(int[][]ary){
+      String s = "[";
+      if (ary.length >= 1){
+        s += arrToString(ary[0]);
+      }
+      if (ary.length > 1){
+        for (int i = 1; i < ary.length; i++){
+          s += ", " + arrToString(ary[i]);
+        }
+      }
+      return s + "]";
+  }
+
+
   public static int countZeros2D(int[][] nums){
     return 0;
   }
 
+  public static int arr2DSum(int[][] nums) {
+    int out = 0;
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = 0; j < nums[i].length; j++) {
+        out += nums[i][j];
+      }
+    }
+    return out;
+  }
+
+  public static void replaceNegative(int[][] vals) {
+  for (int i = 0; i < vals.length; i++) {
+    for (int j = 0; j < vals[i].length; j++) {
+      if (vals[i][j] < 0) {
+        if (i == j) {
+          vals[i][j] = 1;
+        } else {
+          vals[i][j] = 0;
+        public static int arr2DSum(int[][] ary) {
+  int out = 0;
+  for (int i = 0; i < ary.length; i++) {
+    for (int j = 0; j < ary[i].length; j++) {
+      out += ary[i][j];
+    }
+  }
+  return out;
+}
+}
+}
+}
+}
+}
   public static int[][] copy(int[][] nums) {
     int[][] copyArray = new int[nums.length][];
     for (int i = 0; i < nums.length; i++) {
@@ -51,38 +99,9 @@ public class ArrayDemo {
     return swapped;
   }
 
-    public static void replaceNegative(int[][] vals) {
-    for (int i = 0; i < vals.length; i++) {
-      for (int j = 0; j < vals[i].length; j++) {
-        if (vals[i][j] < 0) {
-          if (i == j) {
-            vals[i][j] = 1;
-          } else {
-            vals[i][j] = 0;
-          public static int arr2DSum(int[][] ary) {
-    int out = 0;
-    for (int i = 0; i < ary.length; i++) {
-      for (int j = 0; j < ary[i].length; j++) {
-        out += ary[i][j];
-      }
-    }
-    return out;
-  }
-}
-}
-}
-}
-}
 
-  public static int arr2DSum(int[][] ary) {
-    int out = 0;
-    for (int i = 0; i < ary.length; i++) {
-      for (int j = 0; j < ary[i].length; j++) {
-        out += ary[i][j];
-      }
-    }
-    return out;
-  }
+
+
 
   public static int[][] swapRC(int[][]nums) {
     if (nums.length == 0) {
