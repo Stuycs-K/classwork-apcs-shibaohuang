073aad18de7c373d0695ab43516dc043a305e2dd
Skylar Huang <shibaoh@nycstudents.net>
2024-10-15 22:27:32 -0400

    Committed from classwork

---
+++ 2024-10-15-point/Point.java
@@ -0,0 +1,43 @@
+public class Point{
+  private double x,y;
+
+  public String toString(){
+    return "(" + x + "," + y + ")";
+  }
+
+  //Initialize this Point to have the same values as the other Point.
+  public Point(Point other){
+    //COMPLETE THIS SECOND
+  double pointx = x;
+  double pointy = y;
+  }
+
+  //Initialize this Point to have the provided values
+  public Point(double x, double y){
+    this.x=x;
+    this.y=y;
+  }
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+
+  /*
+  Write this method FOURTH, go to the main and do that one first.
+  */
+  public double distanceTo(Point other){
+    return Math.sqrt( ((this.getX() - other.getX()) * (this.getX- other.getX())) +  ((this.getY() - other.getY()) * (this.getY() - other.getY()) ));
+  }
+
+  /*
+  Write this method last. Re-use (NOT COPY/PASTE) prior work and
+  do not write redundant code.
+  */
+  public static double distance(Point a, Point b){
+    return 0.0;
+  }
+
+
+}
