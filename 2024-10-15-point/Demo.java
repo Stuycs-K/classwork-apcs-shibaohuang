935019ce4147254fb2d2781796021fba1f86abb7
Skylar Huang <shibaoh@nycstudents.net>
2024-10-15 22:43:53 -0400

    Added more test cases to demo

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -17,5 +17,14 @@ public class Demo{
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
+
+    Point p4 = new Point(2, 3);
+    Point p5 = new Point(4, 5);
+    Point p6 = new Point(1, 1);
+    System.out.println(p4);
+    System.out.println(p5);
+    System.out.println(p6);
+    System.out.println(Point.distance(p4, p5));
+    System.out.println(p5.distanceTo(p6));
   }
 }
