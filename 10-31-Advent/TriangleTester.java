70b30c097621604c6502678b0faed3eaafdf8838
Skylar Huang <shibaoh@nycstudents.net>
2024-11-02 18:00:51 -0400

    finished part A

---
+++ 10-31-Advent/TriangleTester.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TriangleTester {
+    public static int countTrianglesA(String filename) {
+        int count = 0;
+        try {
+            File file = new File(filename);
+            Scanner input = new Scanner(file);
+            while (input.hasNextInt()) {
+                int a = input.nextInt();
+                int b = input.nextInt();
+                int c = input.nextInt();
+                if (isValidTriangle(a, b, c)) {
+                    count++;
+                }
+            }
+            input.close();
+        }
+        catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+            }
+            return count;
+    }
+    
+    public static boolean isValidTriangle(int a, int b, int c) {
+        return (a + b > c) && (a + c > b) && (b + c > a);
+    }
+    
+    public static void main(String[] args) {
+        int validTrianglesCount = countTrianglesA("inputTri.txt");
+        System.out.println(validTrianglesCount);
+    }
+}
