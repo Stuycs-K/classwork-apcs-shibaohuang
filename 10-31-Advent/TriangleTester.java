8511003ae66270ec8d7f5504b95878c5b2610107
Skylar Huang <shibaoh@nycstudents.net>
2024-11-02 18:12:34 -0400

    finished and tested part B

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -23,13 +23,37 @@ public class TriangleTester {
             }
             return count;
     }
+
+    public static int countTrianglesB(String filename) {
+        int count = 0;
+        try {
+            File file = new File(filename);
+            Scanner input = new Scanner(file);
+            int[] sides = new int[3];
+            while (input.hasNextLine()) {
+                for (int i = 0; i < 3; i ++) {
+                    if (input.hasNextInt()) {
+                        sides[i] = input.nextInt();
+                    }
+                }
+                if (isValidTriangle(sides[0], sides[1], sides[2])) {
+                    count++;
+                }
+            }
+            input.close();
+        }
+        catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+        }
+        return count;
+    }
     
     public static boolean isValidTriangle(int a, int b, int c) {
         return (a + b > c) && (a + c > b) && (b + c > a);
     }
     
     public static void main(String[] args) {
-        int validTrianglesCount = countTrianglesA("inputTri.txt");
+        int validTrianglesCount = countTrianglesB("inputTri.txt");
         System.out.println(validTrianglesCount);
     }
 }
