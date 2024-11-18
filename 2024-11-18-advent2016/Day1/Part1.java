85b4df1b00a9dc4155ec9eb3545d663cec428a40
Skylar Huang <shibaoh@nycstudents.net>
2024-11-18 18:56:09 -0500

    initialized x, y and direction

--- 2024-11-18-advent2016/Day1/Part1.java
+++ 2024-11-18-advent2016/Day1/Part1.java
@@ -1,9 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Part1 {
-  public static int blocksAway {
-    new
+  public static int blocksAway(String filename) {
+    int x = 0
+    int y = 0;
+    int direction = 0;
   }
 
   public static void main (String[] args) {
-
+    System.out.println(blocksAway("inputBlocks.txt"));
   }
 }
