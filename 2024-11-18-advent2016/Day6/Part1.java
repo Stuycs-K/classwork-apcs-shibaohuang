fa108a98db77fed1223586e70d612f22283aa1af
Skylar Huang <shibaoh@nycstudents.net>
2024-11-19 11:36:31 -0500

    more progress skeleton code (?)

--- 2024-11-18-advent2016/Day6/Part1.java
+++ 2024-11-18-advent2016/Day6/Part1.java
@@ -1,5 +1,22 @@
-public class Part1 {
-  public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Part1 {
+  public static String decipherMessage(String filename) {
+    String message = ""
+    try {
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      input.close()
+    }
+    catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+    }
+    return message;
+    }
+
+    public static void main(String[] args) {
+      System.out.println(decipherMessage("inputSignal.txt"));
+    }
   }
-}
