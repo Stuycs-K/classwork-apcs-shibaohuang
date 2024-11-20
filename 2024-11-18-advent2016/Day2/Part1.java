9fdffc0cbbe404816c0ca75e02c02c1cba12d4ca
Skylar Huang <shibaoh@nycstudents.net>
2024-11-19 22:25:57 -0500

    added code for moving up, down, left, right the keypad

--- 2024-11-18-advent2016/Day2/Part1.java
+++ 2024-11-18-advent2016/Day2/Part1.java
@@ -10,18 +10,29 @@ public class Part1 {
       {4, 5, 6},
       {7, 8, 9}
     };
+    int x = 1;
+    int y = 1;
     try {
       File file = new File(filename);
       Scanner input = new Scanner(file);
+      while (input.hasNextLine()) {
+        String line = input.nextLine();
+        for (char move : line.toCharArray()) {
+          if (move == 'U' && x > 0) x--;
+          if (move == 'D' && x < 2) x++;
+          if (move == 'L' && y > 0) y--;
+          if (move == 'R' && y < 2) y++;
+        }
+        code += keypad[x][y];
+      }
       input.close();
     }
     catch (FileNotFoundException ex) {
       System.out.println("File not found");
     }
-    return message;
-    }
-
+    return code;
+  }
     public static void main(String[] args) {
-      System.out.println(decipherMessage("inputCode.txt"));
+      System.out.println(bathroomCode("inputCode.txt"));
     }
   }
