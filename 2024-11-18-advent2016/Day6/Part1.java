d035910e8263a2da9386a9a55530db3b8d5c2dca
Skylar Huang <shibaoh@nycstudents.net>
2024-11-19 22:06:24 -0500

    made array to count frequency

--- 2024-11-18-advent2016/Day6/Part1.java
+++ 2024-11-18-advent2016/Day6/Part1.java
@@ -17,6 +17,26 @@ public class Part1 {
           rowCount++;
         }
       }
+      if (rowCount == 0) {
+        return message;
+      }
+      int length = lines[0].length();
+      for (int col = 0; col < length; col++) {
+        int[] counts = new int[26];
+        for (int row = 0; row < rowCount; row++) {
+          char ch = lines[row].charAt(col);
+          counts[ch - 'a']++;
+        }
+                
+
+            input.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+        }
+
+        return message;
+    }
+
             
     public static void main(String[] args) {
         System.out.println(decipherMessage("inputSignal.txt"));
