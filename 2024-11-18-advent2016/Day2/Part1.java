080098cd467f65cf5ed34c594b7381a0035fee22
Skylar Huang <shibaoh@nycstudents.net>
2024-11-19 22:23:35 -0500

    initialized keypad

--- 2024-11-18-advent2016/Day2/Part1.java
+++ 2024-11-18-advent2016/Day2/Part1.java
@@ -1,5 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Part1 {
-  public static void main(String[] args) {
-    
+  public static String bathroomCode(String filename) {
+    String code = "";
+    int[][] keypad = {
+      {1, 2, 3},
+      {4, 5, 6},
+      {7, 8, 9}
+    };
+    try {
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      input.close();
+    }
+    catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+    }
+    return message;
+    }
+
+    public static void main(String[] args) {
+      System.out.println(decipherMessage("inputCode.txt"));
+    }
   }
-}
