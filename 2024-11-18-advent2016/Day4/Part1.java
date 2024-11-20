c884791147819db20700389f1b61e9ff669e8902
Skylar Huang <shibaoh@nycstudents.net>
2024-11-19 22:42:29 -0500

    initial skeleton code

--- 2024-11-18-advent2016/Day4/Part1.java
+++ 2024-11-18-advent2016/Day4/Part1.java
@@ -1,5 +1,22 @@
-public class Part1 {
-  public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Part1 {
+  public static String bathroomCode(String filename) {
+    String sum = "";
+    try {
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      input.close();
+    }
+    catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+    }
+    return sum;
+    }
+
+    public static void main(String[] args) {
+      System.out.println(decipherMessage("inputIDs.txt"));
+    }
   }
-}
