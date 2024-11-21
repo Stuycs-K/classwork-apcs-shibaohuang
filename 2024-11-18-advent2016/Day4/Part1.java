18bdb82181fbcbc2a71e4aa5dc40e5b4a5a07ead
Skylar Huang <shibaoh@nycstudents.net>
2024-11-20 20:13:19 -0500

    set variables

--- 2024-11-18-advent2016/Day4/Part1.java
+++ 2024-11-18-advent2016/Day4/Part1.java
@@ -1,14 +1,21 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Part1 {
-  public static String bathroomCode(String filename) {
-    String sum = "";
-    try {
+  public static String decipherMessage(String filename) {
+    int sum = 0;
+    try 
       File file = new File(filename);
       Scanner input = new Scanner(file);
-      input.close();
+      while (input.hasNextLine()) {
+        String line = input.nextLine();
+        int lastDashIndex = line.lastIndexOf('-');
+        int openBracketIndex = line.indexOf('[');
+        String encryptedName = line.substring(0, lastDashIndex);
+        int sectorID = Integer.parseInt(line.substring(lastDashIndex + 1, openBracketIndex));
+        String checksum = line.substring(openBracketIndex + 1, line.length() - 1);
+        int[] frequency = new int[26];
     }
     catch (FileNotFoundException ex) {
       System.out.println("File not found");
