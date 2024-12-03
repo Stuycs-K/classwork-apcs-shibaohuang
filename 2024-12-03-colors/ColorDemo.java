8d81f767520a600b09cb2ddf1c7dbf19d3c0a4e9
Skylar Huang <shibaoh@nycstudents.net>
2024-12-03 11:57:55 -0500

    tested out features

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,5 +1,7 @@
 public class ColorDemo {
   public static void main(String[] args) {
+    System.out.print("\u001b[33mYellow");
+    System.out.print("\u001b[35mMagenta");
     for(int r = 0; r < 256; r+=32) {
       for(int g = 0; g <= 256; g+=32) {
         for(int b = 0; b <= 256; b+=32) {
@@ -8,5 +10,11 @@ public class ColorDemo {
       }
       System.out.println();
     }
+    System.out.print("\u001b[0m");
+    System.out.print("\u001b[31mRed");
+    System.out.print(" \u001b[0mfish ");
+    System.out.print("\u001b[34mBlue");
+    System.out.print(" \u001b[0mfish ");
+    System.out.print("\u001b[0m");
   }
 }
