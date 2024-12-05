445a0471c074f838898fc28150347a284242d16a
Skylar Huang <shibaoh@nycstudents.net>
2024-12-04 21:36:57 -0500

    added display method for even spacing

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -31,9 +31,40 @@ public class Driver {
     }
   }
 
+  public static void displayNumbers(int[] numbers) {
+    Text.go(2, 1);
+    int spacing = 80 / 4;
+    for (int i = 0; i < numbers.length; i++) {
+      if (numbers[i] < 25) {
+        Text.color(Text.RED, Text.BRIGHT);
+      }
+      else if (numbers[i] > 75) {
+        Text.color(Text.GREEN, Text.BRIGHT);
+      }
+      else {
+        Text.color(Text.WHITE);
+      }
+      if (numbers[i] < 10) {
+        System.out.print("0" + numbers[i]);
+      }
+      else {
+        System.out.print(numbers[i]);
+      }
+      if (i < numbers.length - 1) {
+        Text.go(2, (i + 1) * spacing + 1);
+      }
+    }
+  }
+
   public static void main(String[] args) {
     System.out.print(Text.CLEAR_SCREEN);
     System.out.print(Text.HIDE_CURSOR);
+
     border();
+
+    int[] array = randomArray();
+    displayNumbers(array);
+
+    System.out.print(Text.RESET);
   }
 }
