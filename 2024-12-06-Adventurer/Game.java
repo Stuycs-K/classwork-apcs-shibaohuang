cb68d344dbd5cc7890ca55e63599b4dc536788df
Skylar Huang <shibaoh@nycstudents.net>
2024-12-09 19:16:56 -0500

    initiated player & enemy and made helper method for status

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Game {
   public static void main(String[] args) {
     Scanner userInput = new Scanner(System.in);
     System.out.println("Enter username");
     String userName = userInput.nextLine();
     System.out.println("Username is: " + userName);
+    Random random = new Random();
+
+    Adventurer player = new CodeWarrior(userName, 30, "Java");
+    Adventurer enemy = new Fairy("Flora", 50);
+
+    private static void printStatus(Adventurer player, Adventurer enemy) {
+      System.out.println(player.getName() + ", " + player.getHP() + "/" + player.getmaxHP() + " HP, " + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecialName());
+      System.out.println(enemy.getName() + ", " + enemy.getHP() + "/" + enemy.getmaxHP() + " HP, " + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecialName());
+    }
 
     System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
     if (userInput.equals("attack") || userInput.equals("a")) {
