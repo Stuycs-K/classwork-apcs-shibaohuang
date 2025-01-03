353b6e40f2107c3f43621891b4d33d738890f95a
Skylar Huang <shibaoh@nycstudents.net>
2025-01-03 11:41:22 -0500

    added base case

---
+++ 2025-01-02/makeWords.java
@@ -0,0 +1,16 @@
+public class makeWords {
+  public static void makeWords(int remainingLetters, String result, String alphabet) {
+    if (remainingLetters == 0) {
+      System.out.prinln(result);
+    }
+    else {
+      for (int i = 0; i < remainingLetters; i++) {
+        
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    makeWords(3,"","abc");
+  }
+}
